7f5841471bbe5e9e7954d8cde071337cde1837e1
Shadess <tylermboettcher@gmail.com>
2023-04-24 11:28:40 -0400

    Initial setup of site

---
+++ app/src/components/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div class="bg-slate-800 py-6 text-center text-white">
+      <h1 class="text-3xl uppercase">Tyler M Boettcher</h1>
+    </div>
+  );
+}
+
+export default Header;
